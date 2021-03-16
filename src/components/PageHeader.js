dd890dfc1326c2ba27b81d3980866eaaa09de0e3
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-03-16 15:51:39 -0500

    refactored  app's querying, styling to render title, final commit

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,12 +7,10 @@ export default function PageHeader({ handleQueryChange }) {
         <nav className="PageHeader">
           <div className="container d-flex justify-content-center">
             <div className="headerbar-brand ms-auto fas fa-users">
-              <span className="navemp"> Employee Directory </span>
+              <span className="headeremp"> Employee Directory </span>
               <br></br>
-              <span className="headersub">
-                Click to filter by Column or use the Query to
-                narrow your results.
-              </span>
+              
+              
             </div>
           </div>
         </nav>
--- src/utils/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default function getUsers () {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-
